9b557940aeb449f817d11571af4764d18561a439
firassalah <asdfgw@dd>
2023-10-17 19:27:27 +0300

    Fixed UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
-void main() => runApp(XylophoneApp());
+
+void main() => runApp(const XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  void playSound(int soundNumber) {
-    // Implement sound playing logic
+  const XylophoneApp({super.key});
+
+  Future<void> playSound(int soundNumber) async {
+    final player = AudioPlayer();
+    await player.play(AssetSource("note$soundNumber.wav"));
   }
 
+  Expanded buildKey({required Color backgroundColor, required int soundNumber}) {
+    return   Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(backgroundColor: backgroundColor),
+        child: const Text(''),
+        onPressed: () async {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Scaffold(
+      home:Scaffold(
 
-            body: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  child: SizedBox(height: 50,),
-                  onPressed: () async {
-                    final player = AudioPlayer();
-                    await player.play(AssetSource("note1.wav"));
-                  },),
-                ElevatedButton(
-                  child: SizedBox(height: 50,),
-                  onPressed: () async {
-                    final player = AudioPlayer();
-                    await player.play(AssetSource("note2.wav"));
-                  },),
-                ElevatedButton(
-                  child: SizedBox(height: 50,),
-                  onPressed: () async {
-                    final player = AudioPlayer();
-                    await player.play(AssetSource("note3.wav"));
-                  },
-                ),
-              ],
-            ),
+            body: SafeArea(
+              child: Center( child:
+
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  buildKey(backgroundColor: Color(0xFF6A5ACD), soundNumber: 1),
+                  buildKey(backgroundColor: Color(0xFF98FF98), soundNumber: 2),
+                  buildKey(backgroundColor: Color(0xFFF08080), soundNumber: 3),
+                  buildKey(backgroundColor: Color(0xFFDAA520), soundNumber: 4),
+                  buildKey(backgroundColor: Color(0xFFE6E6FA), soundNumber: 5),
+                  buildKey(backgroundColor: Color(0xFF4682B4), soundNumber: 6),
+                  buildKey(backgroundColor: Color(0xFFFA8072), soundNumber: 7),
+                ],
+              ),
           ),
+            ),
       ),
-
     );
   }
 }
